214111a043c98db6415688c684ac5ab17ce426a5
zoominet <zoominet@gmail.com>
2022-07-31 09:39:13 +0800

    element-plus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
+
 const app = createApp(App)
+
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
 
 import pinia from './store'
 app.use(pinia)
